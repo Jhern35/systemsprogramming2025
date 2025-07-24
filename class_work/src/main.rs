fa88fa1d90ca274f027ea5469968e4d99f969ab7
Jhern35 <jonathan.hernandez13@utrgv.edu>
2025-07-24 01:37:30 +0000

    Worked on Structs

--- class_work/src/main.rs
+++ class_work/src/main.rs
@@ -1,14 +1,30 @@
-fn concat_strings(s1: &String, s2: &String) -> String {
-    // Your code here
-    let mut res = "".to_string();
-    res.push_str(s1);
-    res.push_str(s2);
-    res
+struct Car {
+    name:String, 
+    model:String,
+    year:u32,
+}
+
+impl Car {
+    fn new(n:String, m:String, y:u32) -> Car {
+        Car {
+            name:n,
+            model:m,
+            year:y,
+        }
+    }
+
+    fn show_info(&self) {
+        println!("My car maker and models is {}{} and year is {}", self.name, self.model, self.year);
+    }
+
+    fn change_year(&mut self, new_year:u32){
+        self.year = new_year;
+    }
 }
 
 fn main() {
-    let s1 = String::from("Hello, ");
-    let s2 = String::from("World!");
-    let result = concat_strings(&s1, &s2);
-    println!("{}", result); // Should print: "Hello, World!"
+    let mut my_car = Car::new ("Ford".to_string(), "350".to_string(),  2025);
+    my_car.show_info();
+    my_car.change_year(2023);
+    my_car.show_info();
 }
